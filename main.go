4ac474948d7e20dd962e5e8022c1d2af388f4862
bryfry <bryon.fryer@gmail.com>
2022-01-18 00:31:47 +0000

    read envs into config

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"os"
 	"log"
 	"math/rand"
 	"time"
+	"net/http"
 
 	"dips/dnsmasq"
 	"dips/models"
@@ -14,16 +16,45 @@ func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
+func getEnv(key, fallback string) string {
+    if value, ok := os.LookupEnv(key); ok {
+        return value
+    }
+    return fallback
+}
+
 func main() {
-	newHostsChan, err := dnsmasq.InitDnsmasq("/etc/dnsmasq.d")
-	if err != nil {
-		log.Fatal(err)
+
+	newHostsChan := make(chan models.Host)
+	dnsmasqConfig := dnsmasq.Config{
+		DhcpOptionsDir: getEnv("DIPS_DNSMASQ_OPTSDIR","/etc/dnsmasq.d/domains/dhcp-options"),
+		DhcpHostsDir:   getEnv("DIPS_DNSMASQ_HOSTSDIR","/etc/dnsmasq.d/domains/dhcp-hosts"),
+		HostsDir:       getEnv("DIPS_DNSMASQ_HOSTS","/etc/dnsmasq.d/domains/hosts"),
+		NewHostsChan:   newHostsChan,
 	}
-	err = models.InitDB("./dips.db", newHostsChan)
+	dnsmasq.InitDnsmasq(dnsmasqConfig)
+	log.Printf("parsed dnsmasq config: %+v", dnsmasqConfig)
+
+	err := models.InitDB(getEnv("DIPS_DB_PATH","./dips.db"), newHostsChan)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	server := web.InitApp()
+	webConfig := web.Config{
+		Network: getEnv("DIPS_NETWORK", "10.0.0.0/12"),
+		Gateway: getEnv("DIPS_GATEWAY", "10.0.0.1"),
+	        DhcpStartAddress: getEnv("DIPS_DHCP_RANGE_LOW", "10.0.2.1"),
+	        DhcpEndAddress: getEnv("DIPS_DHCP_RANGE_HIGH", "10.15.255.254"),
+	        DhcpLease: getEnv("DIPS_DHCP_LEASE_TIME", "8h"),
+		ListenIP: getEnv("DIPS_LISTEN_INTERFACE","0.0.0.0"),
+		ListenPort: getEnv("DIPS_PORT", "8001"),
+	}
+	r := web.InitApp(webConfig)
+	log.Printf("parsed web config: %+v", webConfig)
+
+	server := &http.Server{
+		Handler: r,
+		Addr:    webConfig.ListenIP + ":" + webConfig.ListenPort,
+	}
 	log.Fatal(server.ListenAndServe())
 }
